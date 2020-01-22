c43058788ed73ad45ea2ee637d8ec699eb5a0a1f
joker-mac <lpinzhou@163.com>
2020-01-23 00:09:24 +0800

    add-function

--- src/main/java/com/joker/tmall/controller/CategoryController.java
+++ src/main/java/com/joker/tmall/controller/CategoryController.java
@@ -1,23 +1,73 @@
 package com.joker.tmall.controller;
 
+import com.joker.tmall.dao.CategoryDao;
 import com.joker.tmall.pojo.Category;
 import com.joker.tmall.service.CategoryService;
+import com.joker.tmall.util.ImageUtil;
 import com.joker.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
+    /**
+     * 分页查询分类列表
+     * @param start
+     * @param size
+     * @return
+     */
     @GetMapping("/categories")
     public Page4Navigator<Category> listCategory(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) {
         return categoryService.listByPage(start, size, 5);
+    }
+
+    /**
+     * 新增分类
+     * @param image
+     * @param request
+     * @param category
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/categories")
+    public Category addCategory(MultipartFile image, HttpServletRequest request, Category category) throws IOException {
+        // 数据入库
+        categoryService.addCategory(category);
+        // 图片服务器
+        saveOrUpdateImageFile(image, request, category);
+        // 返回新的id
+        return category;
+    }
+
+    /**
+     * 把图片存进工程里的指定文件夹
+     * @param image
+     * @param request
+     * @param category
+     * @throws IOException
+     */
+    private void saveOrUpdateImageFile(MultipartFile image, HttpServletRequest request, Category category) throws IOException {
+        File imageFolder = new File(request.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder, category.getId() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        image.transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
     }
 
 }
--- src/main/java/com/joker/tmall/service/CategoryService.java
+++ src/main/java/com/joker/tmall/service/CategoryService.java
@@ -3,6 +3,8 @@ package com.joker.tmall.service;
 import com.joker.tmall.dao.CategoryDao;
 import com.joker.tmall.pojo.Category;
 import com.joker.tmall.util.Page4Navigator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,8 +19,11 @@ public class CategoryService {
     @Autowired
     CategoryDao categoryDao;
 
+    private static Logger LOGGER = LoggerFactory.getLogger(CategoryService.class);
+
+
     /**
-     * 分类-类别查询
+     * 分类-查询
      * @return
      */
     public List<Category> list() {
@@ -26,10 +31,33 @@ public class CategoryService {
         return categoryDao.findAll(sort);
     }
 
+    /**
+     * 分类-分页查询
+     * @param start
+     * @param size
+     * @param navigatePages
+     * @return
+     */
     public Page4Navigator<Category> listByPage(int start, int size, int navigatePages) {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
         Pageable pageable = PageRequest.of(start, size, sort);
         Page<Category> pageFromJPA =categoryDao.findAll(pageable);
         return new Page4Navigator<Category>(pageFromJPA, navigatePages);
+    }
+
+    /**
+     * 分类-新增
+     * @param category
+     */
+    public void addCategory(Category category){
+        categoryDao.save(category);
+    }
+
+    public void deleteCategory(Integer id) {
+        if (id == null || id == 0) {
+            LOGGER.error("传入分类的id为{}", id);
+            throw new NullPointerException("删除操作有误");
+        }
+        categoryDao.deleteById(id);
     }
 }
